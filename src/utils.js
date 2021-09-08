82ac50c80ba4e687ffedbe0be6b6cf8697caea3c
Maciel Barbosa <macielbarbosa@outlook.com>
2021-09-08 00:00:35 -0300

    fix time

--- src/utils.js
+++ src/utils.js
@@ -2,7 +2,11 @@ export const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
 
 export const formatReward = (number) => (number / Math.pow(10, 18)).toFixed(3)
 
+const formatTimeProperty = (property) => (property < 10 ? '0' + property : property)
+
 export const currentTime = () => {
   const date = new Date()
-  return `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+  return `${formatTimeProperty(date.getHours())}:${formatTimeProperty(date.getMinutes())}:${formatTimeProperty(
+    date.getSeconds(),
+  )}`
 }
